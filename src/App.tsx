a83fe49d136a57152846b1d6b44cd3009d52c247
JjayFabor <faborjaylordvhan@gmail.com>
2026-02-08 19:02:58 +0800

    Added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { StoryController } from "./components/StoryController";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
     <div className="w-full h-full">
+      <Analytics />
       <StoryController />
     </div>
   );
