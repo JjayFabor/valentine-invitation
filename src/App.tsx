ebc377e89b2a557dd7d7151b845c8645fa8ec7ee
JjayFabor <faborjaylordvhan@gmail.com>
2026-02-08 19:04:43 +0800

    Added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { StoryController } from "./components/StoryController";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
